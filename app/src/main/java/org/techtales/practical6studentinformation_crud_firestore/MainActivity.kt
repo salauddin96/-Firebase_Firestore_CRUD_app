5e8e654d1b2c52b4853eb5bebe2ef50c2ea8a073
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-11-11 11:19:50 +0600

    initial commit

---
+++ app/src/main/java/org/techtales/practical6studentinformation_crud_firestore/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.techtales.practical6studentinformation_crud_firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practical6(StudentInformation_CRUD_FireStore)"
+include(":app")
